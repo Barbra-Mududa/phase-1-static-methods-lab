95c06e2f22ee7bd9389456d8ff5211c9987b2662
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-09-20 00:17:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    const firstLetter = string[0].toUpperCase()
+    return string.replace(string[0],firstLetter)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9\-\'\ ]/g,"")
+  }
+  static titleize(string){
+    const sentence = string.split(' ');
+    const sentenceCapital= sentence.map(word => {
+      const wordSelected = word.toLowerCase()
+      let result
+      if(wordSelected !== "the" && wordSelected !== "a" && wordSelected !== "an" && wordSelected !== "but" && wordSelected !== "of" && wordSelected !== "and" && wordSelected !== "for" && wordSelected !== "at" && wordSelected !== "by" && wordSelected !== "from") {
+    }else{
+      result = wordSelected
+    }
+    return result
+  })
+  let firstWord = sentenceCapital.join(" ")
+  firstWord = firstWord.replace(firstWord.charAt(0),firstWord.charAt(0).toUpperCase())
+  return firstWord
+  }
 }
