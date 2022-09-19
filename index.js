406ba38f53bbaf3642ab0335603148d5711ea50b
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-09-20 00:22:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ class Formatter {
       const wordSelected = word.toLowerCase()
       let result
       if(wordSelected !== "the" && wordSelected !== "a" && wordSelected !== "an" && wordSelected !== "but" && wordSelected !== "of" && wordSelected !== "and" && wordSelected !== "for" && wordSelected !== "at" && wordSelected !== "by" && wordSelected !== "from") {
-    }else{
+      result = wordSelected.replace(wordSelected.charAt(0),wordSelected.charAt(0).toUpperCase())
+      }else{
       result = wordSelected
-    }
-    return result
-  })
-  let firstWord = sentenceCapital.join(" ")
-  firstWord = firstWord.replace(firstWord.charAt(0),firstWord.charAt(0).toUpperCase())
-  return firstWord
+      }
+      return result
+    })
+    let firstWord = sentenceCapital.join(" ")
+    firstWord = firstWord.replace(firstWord.charAt(0),firstWord.charAt(0).toUpperCase())
+    return firstWord
   }
 }
